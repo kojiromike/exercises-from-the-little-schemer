8b64b847ad2693e5ce09d448c314665c0aa764ca
Michael A. Smith <msmith3@ebay.com>
2015-06-08 23:25:17 -0400

    Chapter 7

--- chapter7.scm
+++ chapter7.scm
@@ -98,3 +98,100 @@
 
 (union '(stewed tomatoes and macaroni casserole) '(macaroni and cheese))
 
+(define intersectall
+  (lambda (l-set)
+    (cond
+      ((null? (cdr l-set)) (car l-set))
+      (else (intersect (car l-set) (intersectall (cdr l-set)))))))
+
+(intersectall '((a b c) (c a d e) (e f g h a b)))
+(intersectall '((6 pears and)
+                (3 peaches and 6 peppers)
+                (8 pears and 6 plums)
+                (and 6 prunes with some apples)))
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define a-pair?
+  (lambda (s-exp)
+    (and (not (or (atom? s-exp)
+                  (null? s-exp)
+                  (null? (cdr s-exp))))
+         (null? (cdr (cdr s-exp))))))
+
+(a-pair? '(pear pear))
+(a-pair? '(3 7))
+(a-pair? '((2) (pair)))
+(a-pair? '(full (house)))
+(a-pair? '())
+(a-pair? '(a))
+(a-pair? '(() ()))
+(a-pair? '(() () ()))
+(a-pair? 'abc)
+
+(define first
+  (lambda (p)
+    (car p)))
+
+(define second
+  (lambda (p)
+    (car (cdr p))))
+
+(define build
+  (lambda (s1 s2)
+    (cons s1 (cons s2 '()))))
+
+(define third
+  (lambda (p)
+    (car (cdr (cdr p)))))
+
+(define firsts
+  (lambda (l)
+    (cond
+      ((null? l) '(car (car l)))
+      (else (cons (car (car l)) (firsts (cdr l)))))))
+
+(define fun?
+  (lambda (rel)
+    (set? (firsts rel))))
+
+(fun? '((8 3) (4 2) (7 6) (6 2) (3 4)))
+(fun? '((d 4) (b 0) (b 9) (e 5) (g 4)))
+
+(define revrel-orig
+  (lambda (rel)
+    (cond
+      ((null? rel) '())
+      (else (cons (build (second (car rel)) (first (car rel)))
+                  (revrel-orig (cdr rel)))))))
+
+(revrel-orig '((8 a) (pumpkin pie) (got sick)))
+
+(define revpair
+  (lambda (pair)
+    (build (second pair) (first pair))))
+
+(define revrel-revpair
+  (lambda (rel)
+    (cond
+      ((null? rel) '())
+      (else (cons (revpair (car rel))
+                  (revrel-revpair (cdr rel)))))))
+
+(define seconds
+  (lambda (rel)
+    (cond
+      ((null? rel) '())
+      (else (cons (second (car rel))
+                  (seconds (cdr rel)))))))
+
+(define fullfun?
+  (lambda (fun)
+    (set? (seconds fun))))
+
+(fullfun? '((8 3) (4 2) (7 6) (6 2) (3 4)))
+(fullfun? '((8 3) (4 8) (7 6) (6 2) (3 4)))
+(fullfun? '((grape raisin) (plum prune) (stewed prune)))
+(fullfun? '((grape raisin) (plum prune) (stewed grape)))
